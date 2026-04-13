3207d755ef0f07c09932075f987e66cb4a91f6ef
hopexon <hopexon@gmail.com>
2026-04-13 20:38:14 +0900

    wip update tips / add layers at global.css

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,10 @@
 import type { MDXComponents } from "mdx/types"
 import CodeBlock from '@/components/tips/CodeBlock'
+import styles from '@/app/tips/tips.module.css'
 
 const components: MDXComponents = {
   pre: CodeBlock,
+  p: ({ children }) => <p className={styles.tips__base__txt}>{children}</p>
 }
 
 export function useMDXComponents(): MDXComponents {
